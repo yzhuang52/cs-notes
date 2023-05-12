823fb917001db6b8285d5ecb900996454e077906
littlekimber <zhyan0096@gmail.com>
2023-05-12 18:58:41 +0000

    finish cs61c lab01

--- cs61c/lab/lab01/ll_cycle.c
+++ cs61c/lab/lab01/ll_cycle.c
@@ -2,6 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *runner = head;
+    node *walker = head;
+    while (runner != NULL && runner->next != NULL) {
+        runner = runner->next->next;
+        walker = walker->next;
+        if (runner == walker) {
+            return 1;
+        }
+    }
     return 0;
 }
