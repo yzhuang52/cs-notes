1f5a27f78f85629ffef5af345b75eb2bb5715b1b
littlekimber <zhyan0096@gmail.com>
2023-05-14 15:49:09 +0000

    finish partA

--- cs61c/proj/proj1/steganography.c
+++ cs61c/proj/proj1/steganography.c
@@ -21,13 +21,55 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+	Color* cell = *(image->image + row * image->cols + col);
+	Color* result = (Color*) malloc(sizeof(Color));
+	if (result == NULL)
+	{
+		printf("Malloc fail\n");
+		exit(-1);
+	}
+	if ((cell->B&1) == 1)
+	{
+		result->R = 255;
+		result->G = 255;
+		result->B = 255;
+	} else
+	{
+		result->R = 0;
+		result->G = 0;
+		result->B = 0;
+	}
+	return result;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+	Image* im = (Image*) malloc(sizeof(Image));
+	if (im == NULL)
+	{
+		printf("Malloc fail\n");
+		exit(-1);
+	}
+	im->rows = image->rows;
+	im->cols = image->cols;
+	int total_pixels = im->rows * im->cols;
+	im->image = (Color**) malloc(sizeof(Color*)*total_pixels);
+	if (im->image == NULL)
+	{
+		printf("Malloc fail\n");
+		exit(-1);
+	}
+	Color** p = im->image;
+	for (int i = 0; i < im->rows; i++)
+	{
+		for (int j = 0; j < im->cols; j++)
+		{
+			*p = evaluateOnePixel(image, i, j);
+			p++;
+		}
+	}
+	return im;
 }
 
 /*
@@ -45,5 +87,17 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if (argc != 2) 
+	{
+		printf("Wrong usage!\n");
+		exit(-1);
+	}
+	Image* image = readData(argv[1]);
+	Image* steImage = steganography(image);
+	writeData(steImage);
+	freeImage(image);
+	freeImage(steImage);
+	return 0;
 }
+
+
