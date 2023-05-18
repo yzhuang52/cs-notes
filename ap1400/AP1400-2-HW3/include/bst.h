2a3275c9f36d411a1d32719d919f419bc07f90ad
littlekimber <zhyan0096@gmail.com>
2023-05-18 20:10:21 +0000

    ap1400 finish node class

--- ap1400/AP1400-2-HW3/include/bst.h
+++ ap1400/AP1400-2-HW3/include/bst.h
@@ -1,31 +1,55 @@
 #ifndef BST_H
 #define BST_H
+#include <iostream>
+#include <functional>
 
-class Node
-{
-public:
-	Node(int value, Node* left, Node* right);
-	Node();
-	Node(const Node& node);
-
-	int value;
-	Node* left;
-	Node* right;
-};
 
 class BST
 {
-public:
+    public:
+    class Node;
+    BST();
+    BST(BST& bst);
+    BST(BST&& bst);
     Node*& get_root();
     void bfs(std::function<void(Node*& node)> func);
-    size_t length();
+    std::size_t length() const;
     bool add_node(int value);
-    Node** find_node(int value);
-    Node** find_parrent(int value);
-    Node** find_successor(int value);
+    Node** find_node(int value) const;
+    Node** find_parrent(int value) const;
+    Node** find_successor(int value) const;
     bool delete_node(int value);
-
+    ~BST();
 private:
     Node* root;
 };
+
+class BST::Node
+{
+public:
+    Node(int value, Node* left, Node* right);
+    Node();
+    Node(const Node& node);
+
+    int value;
+    Node* left;
+    Node* right;
+};
+
+bool operator>(const BST::Node& node, int num);
+bool operator>=(const BST::Node& node, int num);
+bool operator<(const BST::Node& node, int num);
+bool operator<=(const BST::Node& node, int num);
+bool operator==(const BST::Node& node, int num);
+bool operator<(int num, const BST::Node& node);
+bool operator<=(int num, const BST::Node& node);
+bool operator>(int num, const BST::Node& node);
+bool operator>=(int num, const BST::Node& node);
+bool operator==(int num, const BST::Node& node);
+std::ostream& operator<<(std::ostream& out, const BST::Node& node);
+std::ostream& operator<<(std::ostream& out, const BST& bst);
+void operator=(BST& bst1, BST& bst2);
+void operator=(BST& bst1, BST&& bst2);
+BST& operator++(BST& bst1);
+BST operator++(BST& bst1, int);
 #endif //BST_H
--- ap1400/AP1400-2-HW3/src/bst.cpp
+++ ap1400/AP1400-2-HW3/src/bst.cpp
@@ -1,23 +1,124 @@
 #include "bst.h"
 
-Node::Node(int value, Node* left, Node* right) {
+BST::Node::Node(int value, Node* left, Node* right) {
     this->value = value;
     this->left = left;
     this->right = right;
 }
 
-Node::Node() {
+BST::Node::Node() {
     this->left = nullptr;
     this->right = nullptr;
     this->value = 0;
 }
 
-Node::Node(const Node& node) {
+BST::Node::Node(const Node& node) {
     this->value = node.value;
     this->left = node.left;
     this->right = node.right;
 }
 
+bool operator>(const BST::Node& node, int num) {
+    return node.value > num;
+}
+
+bool operator>=(const BST::Node& node, int num) {
+    return node.value >= num;
+}
+
+bool operator<(const BST::Node& node, int num) {
+    return node.value < num;
+}
+
+bool operator<=(const BST::Node& node, int num) {
+    return node.value <= num;
+}
+
+bool operator==(const BST::Node& node, int num) {
+    return node.value == num;
+}
+
+bool operator<(int num, const BST::Node& node) {
+    return num < node.value;
+}
+
+bool operator<=(int num, const BST::Node& node) {
+    return num <= node.value;
+}
+
+bool operator>(int num, const BST::Node& node) {
+    return num > node.value;
+}
+
+bool operator>=(int num, const BST::Node& node) {
+    return num >= node.value;
+}
+
+bool operator==(int num, const BST::Node& node) {
+    return num == node.value;
+}
+
+std::ostream& operator<<(std::ostream& out, const BST::Node& node) {
+    out << &node << "  " << node.value << "  " << &node.left  << "  " << &node.right << std::endl;
+    return out;
+}
+
+BST::BST() {
+    this->root = nullptr;
+}
+
+BST::BST(BST& bst) {
+    this->root = bst.root;
+}
+
+BST::BST(BST&& bst) {
+    this->root = bst.root;
+}
+
+BST::Node*& BST::get_root() {
+    return this->root;
+}
+
+void BST::bfs(std::function<void(BST::Node*& node)> func) {
+
+}
+
+std::size_t BST::length() const {
+    return 1;
+}
+
+bool BST::add_node(int value) {
+    return 1;
+}
+
+BST::Node** BST::find_node(int value) const {
+    return nullptr;
+}
+
+BST::Node** BST::find_parrent(int value) const {
+    return nullptr;
+}
+
+BST::Node** BST::find_successor(int value) const {
+    return nullptr;
+}
+
+bool BST::delete_node(int value) {
+    return true;
+}
+
+std::ostream& operator<<(std::ostream& out, BST& bst) {
+
+    return out;
+}
+
+void operator=(BST& bst1, BST& bst2) {
+    bst1.root = bst2.root;
+}
+
+void operator=(BST& bst1, BST&& bst2) {
+    bst1.root = bst2.root;
+}
 
 
 
