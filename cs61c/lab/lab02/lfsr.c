494cc6216a0d3049745fbea01815e8c3d39b15c1
littlekimber <zhyan0096@gmail.com>
2023-05-18 04:20:38 +0000

    cs61c lab02 finish

--- cs61c/lab/lab02/lfsr.c
+++ cs61c/lab/lab02/lfsr.c
@@ -4,7 +4,24 @@
 #include <string.h>
 #include "lfsr.h"
 
+// int next_head(uint16_t *reg) {
+// 	int x = 0;
+// 	x ^= ((*reg)&(1<<0));
+// 	x ^= (((*reg))&(1<<2));
+// 	x ^= (((*reg))&(1<<3));
+// 	x ^= (((*reg))&(1<<5));
+// 	return x;
+// }
+// void lfsr_calculate(uint16_t *reg) {
+//     /* YOUR CODE HERE */
+// 	int head = next_head(reg);
+// 	(*reg) >>= 1;
+// 	(*reg) |= (head << 15);
+// }
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned bit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1u;
+    *reg = (*reg >> 1) | (bit << 15);
 }
 
