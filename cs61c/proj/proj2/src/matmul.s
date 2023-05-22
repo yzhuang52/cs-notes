d425a95135ee8a922b8126a5d20356184ff3101d
littlekimber <zhyan0096@gmail.com>
2023-05-22 03:24:54 +0000

    cs61c proj2 finish partA

--- cs61c/proj/proj2/src/matmul.s
+++ cs61c/proj/proj2/src/matmul.s
@@ -26,38 +26,82 @@
 matmul:
 
     # Error checks
-
-
+    li t0, 1
+    # Checks the size of m0
+    blt a1, t0, exit_m0
+    blt a2, t0, exit_m0
+    # Checks the size of m1
+    blt a4, t0, exit_m1
+    blt a5, t0, exit_m1
+    # Checks if m0 and m1 size matches
+    bne a2, a4, exit_nomatch
     # Prologue
-
-
+    addi sp, sp -4
+    sw ra, 0(sp)
+    li t0, 0 # t0 hold the outer loop index, initialize as 0
+    mv t2, a6 # t2 hold the address of result[i][j]
 outer_loop_start:
-
-
-
-
+    beq t0, a1, outer_loop_end 
+    li t1, 0 # t1 hold the inner loop index, initialize as 0
 inner_loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    # t4 = col, t5 = rowi * colm1
+    # t6 hold the index of result
+    beq t1, a5, inner_loop_end
+    slli t3, t0, 2
+    slli t4, t1, 2
+    mul t5, t3, a5
+    add t6, t5, t4
+    addi sp, sp, -40
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+    sw a4, 16(sp)
+    sw t0, 20(sp)
+    sw t1, 24(sp)
+    sw t2, 28(sp)
+    sw a5, 32(sp)
+    sw s0, 36(sp)
+    # adjust the arguments for dot function
+    mul s0, t3, a2
+    add a0, a0, s0
+    mv a1, a3
+    add a1, a1, t4 
+    li a3, 1
+    mv a4, a5
+    jal dot
+    lw t0, 20(sp)
+    lw t1, 24(sp)
+    lw t2, 28(sp)
+    sw a0, 0(t2)
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    lw a4, 16(sp)
+    lw a5, 32(sp)
+    lw s0, 36(sp)
+    addi sp, sp, 40
+    addi t1, t1, 1
+    addi t2, t2, 4
+    jal inner_loop_start
 inner_loop_end:
-
-
-
-
+    addi t0, t0, 1
+    jal outer_loop_start
 outer_loop_end:
-
-
     # Epilogue
-    
-    
+    lw ra, 0(sp)
+    addi sp, sp, 4
     ret
+
+exit_m0:
+    addi a1, x0, 72
+    j exit2 
+
+exit_m1:
+    addi a1, x0, 73
+    j exit2 
+
+exit_nomatch:
+    addi a1, x0, 74
+    j exit2
--- cs61c/proj/proj2/unittests/unittests.py
+++ cs61c/proj/proj2/unittests/unittests.py
@@ -277,16 +277,19 @@ class TestMatmul(TestCase):
         array_out = t.array([0] * len(result))
 
         # load address of input matrices and set their dimensions
-        raise NotImplementedError("TODO")
-        # TODO
+        t.input_array("a0", array0)
+        t.input_scalar("a1", m0_rows)
+        t.input_scalar("a2", m0_cols)
+        t.input_array("a3", array1)
+        t.input_scalar("a4", m1_rows)
+        t.input_scalar("a5", m1_cols)
         # load address of output array
-        # TODO
-
+        t.input_array("a6", array_out)
         # call the matmul function
         t.call("matmul")
 
         # check the content of the output array
-        # TODO
+        t.check_array(array_out, result)
 
         # generate the assembly file and run it through venus, we expect the simulation to exit with code `code`
         t.execute(code=code)
@@ -296,6 +299,48 @@ class TestMatmul(TestCase):
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [30, 36, 42, 66, 81, 96, 102, 126, 150]
+        )
+
+    def test_moresimple(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3, 4,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 4, 3,
+            [70, 80, 90, 158, 184, 210, 246, 288, 330]
+        )
+    def test_m0_row_size(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 3,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
+            [30, 36, 42, 66, 81, 96, 102, 126, 150],
+            code=72
+        )
+    def test_m0_col_size(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 0,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
+            [30, 36, 42, 66, 81, 96, 102, 126, 150],
+            code=72
+        )
+    def test_m1_row_size(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 3,
+            [30, 36, 42, 66, 81, 96, 102, 126, 150],
+            code=73
+        )
+    def test_m1_col_size(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 0,
+            [30, 36, 42, 66, 81, 96, 102, 126, 150],
+            code=73
+        )
+    def test_match(self):
+        self.do_matmul(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 1,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 2,
+            [30, 36, 42, 66, 81, 96, 102, 126, 150],
+            code=74
         )
 
     @classmethod
